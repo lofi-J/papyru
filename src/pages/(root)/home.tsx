79a03b68db4063d7831e2f85134fe9657d560448
lofi <jsj2505@gmail.com>
2025-06-26 15:18:57 +0900

    vite-plugin-svgr

--- src/pages/(root)/home.tsx
+++ src/pages/(root)/home.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router';
+import Sparkles from '../../assets/sparkles.svg?react';
 
 export default function Home() {
   const navigate = useNavigate();
@@ -6,6 +7,8 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-2">
       <h1>Home</h1>
+      <Sparkles className="size-4" fill="red" />
+      <Sparkles className="size-10" fill="blue" />
       <button onClick={() => navigate('/123123')}>404</button>
     </div>
   );

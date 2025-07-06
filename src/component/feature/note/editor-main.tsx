fa80609dcdd5132a8b7c1c753318777ff96f1afa
lofi <jsj2505@gmail.com>
2025-07-06 14:53:49 +0900

    [design] editor에서는 user-select 가능

--- src/component/feature/note/editor-main.tsx
+++ src/component/feature/note/editor-main.tsx
@@ -1,5 +1,9 @@
 import { PropsWithChildren } from 'react';
 
 export default function EditorMain({ children }: PropsWithChildren) {
-  return <div className="w-full h-full editor-main">{children}</div>;
+  return (
+    <div data-editor="true" className="w-full h-full editor-main">
+      {children}
+    </div>
+  );
 }
